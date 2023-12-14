2ffaabb765f594c330c692ac4a4e4d4b364d464e
balennouri <balennouri@gmail.com>
2023-12-14 17:15:08 +0100

    Add my questions to question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,114 @@
+/* Creating an Array containing all the questions and answers in the boolean format */
+console.log('test')
+let questions = [{
+    numb: 1,
+    question: "Which player has scored the most goals in Champions League history?",
+    answer: "Cristiano Ronaldo",
+    options: [
+      "Leo Messi",
+      "Cristiano Ronaldo",
+      "Robert Lewandowski",
+      "Zlatan Ibrahimovic"
+
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which player has the most appearances in Champions League history?",
+    answer: "Iker Casillas",
+    options: [
+      "Xavi",
+      "Iker Casillas",
+      "Raúl",
+      "Cristiano Ronaldo"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following players has scored the most goals in the Champions League?",
+    answer: "Zlatan Ibrahimovic",
+    options: [
+      "Didier Drogba",
+      "Robert Lewandowski",
+      "Zlatan Ibrahimovic",
+      "Filippo Inzaghi"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Who has won the Champions League with the most different clubs (three)?",
+    answer: "Clarence Seedorf",
+    options: [
+      "Samuel Eto'o",
+      "Clarence Seedorf",
+      "Cristiano Ronaldo",
+      "Toni Kroos"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which team has scored the most goals (25, season 2017/2018) in a Champions League group stage?",
+    answer: "PSG",
+    options: [
+      "PSG",
+      "Real Madrid",
+      "Manchester City",
+      "Monaco"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How many goals did Cristiano Ronaldo score in the 2013/2014 season when he broke a new record for goals in one season in the Champions League?",
+    answer: "17",
+    options: [
+      "15",
+      "16",
+      "17",
+      "18"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who has the most assists in Champions League history?",
+    answer: "Ryan Giggs",
+    options: [
+      "Leo Messi",
+      "Ryan Giggs",
+      "Xavi",
+      "Mesut Özil"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many hattricks have Cristiano Ronaldo and Leo Messi scored in the Champions Leauge?",
+    answer: "8",
+    options: [
+      "4",
+      "6",
+      "8",
+      "10"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which player has represented the most clubs in the Champions League group stage?",
+    answer: "Zlatan Ibrahimovic",
+    options: [
+      "Zlatan Ibrahimovic",
+      "Javier Saviola",
+      "Samuel Eto'o",
+      "Hernán Crespo"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who has scored the fastest goal in Champions League history?",
+    answer: "Roy Makaay",
+    options: [
+      "Raúl",
+      "Fernando Torres",
+      "Hernán Crespo",
+      "Roy Makaay"
+    ]
+  }
+];
